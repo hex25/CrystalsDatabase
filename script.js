56f17cdb631a2347d007c21b26556590b47e7b47
Natasha Minin <natasha.minin@gmail.com>
2020-02-09 12:34:38 +0100

    Crystals added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
         planet : [''],
         element : [''],
         chakra : [''],
+        imagePath: ['./images/'],
+        application: [''],
         info : [''],
     }, */
 
@@ -14,9 +16,9 @@ const crystals = [
         zodiac: [' Virgo', ' Sagittarius', ' Capricorn', ' Aquarius', ' Pisces'],
         planet: [' Jupiter'],
         element: [' Air', ' Water'],
-        chakra: [' Third Eye Chakra', ' Crown Chakra'],
+        chakra: [' Third Eye', ' Crown'],
         imagePath: ['./images/amethyst.jpg'],
-        application: ['Test1','Test2','Test3'],
+        application: [' Nightmares',' Insomnia',' Emotional Issues',' Nervous System'],
         info: ' Amethyst is a powerful and protective stone.  It guards against psychic attack, transmuting the energy into love and protecting the wearer from all types of harm, including geopathic or electromagnetic stress and ill wishes from others.  Amethyst is a natural tranquiliser, it relieves stress and strain, soothes irritability, balances mood swings, dispels anger, rage, fear and anxiety.  Alleviates sadness and grief, and dissolves negativity.  Amethyst activates spiritual awareness, opens intuition and enhances psychic abilities.  It has strong healing and cleansing powers.  Amethyst encourages sobriety, having a sobering effect on overindulgence of alcohol, drugs or other addictions.  It calms and stimulates the mind, helping you become more focused, enhancing memory and improving motivation.  Amethyst assists in remembering and understanding dreams.  It relieves insomnia.  Encourages selflessness and spiritual wisdom. Amethyst boosts hormone production, tunes the endocrine system and metabolism.  It strengthens the immune system, reduces pain and strengthens the body to fight against cancer. It destroys malignant tumours and aids in tissue regeneration.  Cleanses the blood.  Relieves physical, emotional and psychological pain or stress.  Amethyst eases headaches and releases tension.  It reduces bruising, swellings, injuries, and treats hearing disorders.  Amethyst heals diseases of the lungs and respiratory tract, skin conditions, cellular disorders and diseases of the digestive tract.',
     },
     {
@@ -24,9 +26,9 @@ const crystals = [
         zodiac: [' Taurus', ' Libra'],
         planet: [' Venus'],
         element: [' Earth', ' Water'],
-        chakra: [' Heart Chakra'],
+        chakra: [' Heart'],
         imagePath: ['./images/rose_quartz.jpeg'],
-        application: ['Test2'],
+        application: [' Heart', ' Jealousy', ' Anger', ' Resentment', ' Romance', ' Love'],
         info: [' Rose Quartz is the stone of universal love.  It restores trust and harmony in relationships, encouraging unconditional love.  Rose Quartz purifies and opens the heart at all levels to promote love, self-love, friendship, deep inner healing and feelings of peace.  Calming and reassuring, it helps to comfort in times of grief.  Rose Quartz dispels negativity and protects against environmental pollution, replacing it with loving vibes.  It encourages self forgiveness and acceptance invoking self trust and self worth. Rose Quartz strengthens and balances the physical heart and circulatory system, and releases impurities from body fluids.  It hastens recovery, reduces high blood pressure, aids chest and lung problems, heals the kidneys and adrenals, and alleviates vertigo.  A good energetic support for those with leukaemia. Rose Quartz has the power to increase fertility and to protect both mother and unborn fetus from miscarriage.'],
     },
     {
@@ -34,10 +36,60 @@ const crystals = [
         zodiac: [' Gemini', ' Leo'],
         planet: [' Saturn'],
         element: [' Earth'],
-        chakra: [' Root Chakra'],
+        chakra: [' Root'],
         imagePath: ['./images/black_onyx.jpg'],
-        application: ['Test2','Test3','Test4'],
+        application: [' Happiness',' Good Fortune',' Negative Energy', ' Grounding', ' Strength', ' Self Discipline'],
         info: [' Onyx gives strength.  It promotes vigor, steadfastness and stamina.  Imparts self-confidence, helping you to be at ease in your surroundings.  Onyx banishes grief, enhances self-control and stimulates the power of wise decision-making.  It encourages happiness and good fortune. Onyx treats disorders of the bones, bone marrow and blood.  It is beneficial for teeth and the feet.'],
+    },
+    {
+        name : ['Malachite'],
+        zodiac : [' Capricorn', ' Scorpio'],
+        planet : [''],
+        element : [''],
+        chakra : [' Heart', ' Solar Plexus'],
+        imagePath: ['./images/malachite.jfif'],
+        application: [' Confidence', ' Heart', ' Creativity',' Leadership', ' Protection'],
+        info : ['Malachite is beautiful, rich green copper carbonate hydroxide mineral that has been mined and smelted in Israel for more than 3,000 years. It has also been found in Russia, Gabon, the Democratic Republic of Congo, Namibia, Zambia, Tsumeb, Mexico, New South Wales, France, and the southwestern United States. At deposit sites, malachite is commonly found along with goethite, calcite, and azurite where copper ores have been weathered. It exhibits a monoclinic crystal system, a translucent to opaque diaphaneity, a Moh\'s scale hardness of 3.5 to 4, a specific gravity of 3.6 to 4, and a range of possibilities for luster depending on the type of specimen (massive, fibrous, etc.). Malachite has been used as a source of pigment, as a gemstone, and as an ornamental stone. Several famous decorative vases have been made of malachite. It is important to note that malachite should only be handled in its finished, polished form, as it is a toxic substance. Its dust should not be breathed in. On a metaphysical level, malachite represents the deep healing green of Mother Earth and the beauty we see in nature every day. It is also a stone of transformation and protection. It is supposed to protect us from negative energies and pollution. Wearers and carriers are advised to keep them near televisions, microwaves, or in the workplace. It may guard against electromagnetic pollution, radiation,  negative e-mails or phone calls, over bright lighting, noise pollution and other damaging rays. It might come in handy in particular if you are a secretary, miner, or airline worker, as it is said to promote clear thinking and protection from accidents. If you get nervous when you have to travel, keeping a specimen of malachite with you might help to ease your nerves. It is supposed to encourage safe and smooth travel whether by car or airplane. Just hold the malachite before you leave for your trip, to empower it, and you will have an amulet of protection as a traveling partner. It is also called a stone of transformation because it may allow you to see and break blockages that stand in the way of your spiritual growth, and encourages emotional risk-taking and change. Malachite is said to emit physical healing energies, as well. It is sometimes referred to as the midwife stone, as it is often used to regulate the menstrual cycle and PMS symptoms (such as cramps), ease labor, and treat sexual disease. It is also believed to provide healing benefits for those suffering from malaria, cold sweats, Parkinson\'s disease, rheumatic pain, intestinal disease, asthma, gall and kidney stones, high blood pressure, vertigo and motion sickness, epilepsy, broken bones and torn muscles, growths or tumors, swollen joints, or a weak or compromised immune system. If you have a liver that it not functioning properly, the presence of malachite may help stimulate it to release harmful toxins.'],
+    },
+    {
+        name : ['Moonstone'],
+        zodiac : [' Cancer', ' Libra', ' Scorpio'],
+        planet : [''],
+        element : [''],
+        chakra : [' Third Eye', ' Crown'],
+        imagePath: ['./images/moonstone.jfif'],
+        application: [' Intuition', ' Dreams', ' Insight'],
+        info : ['Moonstone is a beautiful shimmery stone that has always been surrounded by a great deal of magic and mystique. It is even regarded as holy in many cultures. In India, it is regarded as a dream stone that brings its wearer beautiful visions during the night. In Arabic countries, it is sewn into the garments of women, as it is believed to bring fertility. The Greeks and Romans associated it with their lunar deities. The most classic specimens come from Sri Lanka, but moonstone has also been found in Australia, Brazil, Myanmar, Madagascar, the United States, and other locations. It is made up of orthoclase and albite-- two species of feldspar. In its creation, these feldspars are intermingled and then separate into stacked and alternating layers as the mineral cools. These stones are almost always cut as cabochons, but sometimes they are also made into cameos and/or engraved. For centuries, they have been used in jewelry. Their famous mysterious shimmer (or adularescence, as it is called in the trade) is mostly brought to bear by the skill of the cutter. Moonstone is a sodium potassium aluminum silicate that exists in numerous colors and exhibits a specific gravity of 2.61, a white streak, an opalescent luster and an uneven to conchoidal fracture. Its Moh\'s scale hardness of 6 means that it must be handled with care. Moonstone is associated with the moon (of course), the element of wind, the 13th wedding anniversary, the third eye and crown chakras, the zodiac signs of cancer and sagittarius, the months of April, June, August and October, and the state of Florida (although it does not occur there naturally). It is believed to give off energies of luck, power, healing and love. Simply keep moonstone with you or wear it in the form of jewelry in order to allow it to influence your vibrational energy. This may entail protection at sea and on land, success in matters of business and love, incoming of inspiration, enhancement of intuition and the personality, increased ability to foretell the future, and overall good fortune. It is said that  you can guarantee passion in your relationship with your lover forever if you gift him or her with a moonstone necklace during a full moon. Moonstone is also believed to be a great aid in meditation and an excellent stone for teenagers and women, in particular. In the world of physical healing, crystal healers have used moonstone to maintain the biological rhythms of the body, bring balance to the endocrine system, reduce fluid retention, promote growth in children and teenagers, slow degeneration of the body due to the aging process, enhance nutrient assimilation, eliminate toxins, and treat insomnia and conditions of the digestive system, eyes, skin, hair and fleshy organs. Additionally, it is believed to have a profound effect on the female reproductive system, easing menstrual and menopause problems, increasing fertility, facilitating healthy pregnancy and childbirth, and more.'],
+    },
+    {
+        name : ['Tiger Eye'],
+        zodiac : [''],
+        planet : [''],
+        element : [''],
+        chakra : [' Sacral', ' Third Eye'],
+        imagePath: ['./images/tigereye.jfif'],
+        application: [' Protection',' Good Luck',' Intuition',' Mental Health',' Prosperity'],
+        info : [''],
+    },
+    {
+        name : ['Lapis Lazuli'],
+        zodiac : [''],
+        planet : [''],
+        element : [''],
+        chakra : [''],
+        imagePath: ['./images/lapis.jfif'],
+        application: [''],
+        info : [''],
+    },
+    {
+        name : ['Turquoise'],
+        zodiac : [''],
+        planet : [''],
+        element : [''],
+        chakra : [''],
+        imagePath: ['./images/turquoise.jfif'],
+        application: [''],
+        info : [''],
     },
     /* template, copy paste from { to }, 
     {
@@ -46,6 +98,8 @@ const crystals = [
         planet : [''],
         element : [''],
         chakra : [''],
+        imagePath: ['./images/'],
+        application: [''],
         info : [''],
     },
     */
@@ -55,6 +109,7 @@ const crystals = [
 // formatting crystal info
 function formatCrystal(crystal, container) {
     let crystalHeader = document.createElement("H2");
+    crystalHeader.setAttribute('class', 'crystal-header');
     crystalHeader.innerHTML = crystal.name;
     let crystalImage = document.createElement("IMG");
     crystalImage.src = crystal.imagePath;
